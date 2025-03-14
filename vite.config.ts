1c0378619e669f2f93f9cbd28bd9f3e16782b9dc
Diogo-Lages <diogo.m.lages@gmail.com>
2025-03-14 23:49:29 +0000

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -25,11 +25,11 @@ export default defineConfig({
   base: "/Blueteam-Framework",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: path.resolve(__dirname),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
